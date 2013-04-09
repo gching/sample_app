9b2a0e430324c8012da58796708f01942b355ba0
Gavin Ching <gavinchingy@gmail.com>
2013-04-09 03:46:05 -0400

    Finish Sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
 
   before_save {email.downcase!}
 
+  before_save :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -22,4 +24,11 @@ class User < ActiveRecord::Base
 
   validates :password, length: {minimum: 6}
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+    
 end
